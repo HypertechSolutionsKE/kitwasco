d0872ad89f7d45da3833b223cc947d520a6d0dbb
Ken Mbuki <ken.mbuki@savannahinformatics.com>
2017-08-31 15:59:21 +0300

    Hooking up index page - assets, controllers and other site configs

---
+++ application/views/fe/home.php
@@ -0,0 +1,326 @@
+<div role="main" class="main">
+    <div class="slider-container light rev_slider_wrapper">
+        <div id="revolutionSlider" class="slider rev_slider"
+        data-plugin-revolution-slider data-plugin-options="{'delay': 9000, 'gridwidth': 1170, 'gridheight': 500, 'disableProgressBar': 'on'}">
+            <ul>
+                <li data-transition="fade">
+                    <img src="<?php echo base_url();?>assets/img/slides/slide-corporate-1.jpg"
+                        alt=""
+                        data-bgposition="center center"
+                        data-bgfit="cover"
+                        data-bgrepeat="no-repeat"
+                        data-kenburns="on"
+                        data-duration="9000"
+                        data-ease="Linear.easeNone"
+                        data-scalestart="150"
+                        data-scaleend="100"
+                        data-rotatestart="0"
+                        data-rotateend="0"
+                        data-offsetstart="0 0"
+                        data-offsetend="0 0"
+                        data-bgparallax="0"
+                        class="rev-slidebg">
+
+                    <div class="tp-caption"
+                        data-x="177"
+                        data-y="188"
+                        data-start="1000"
+                        data-transform_in="x:[-300%];opacity:0;s:500;">
+                        <img src="<?php echo base_url();?>assets/img/slides/slide-title-border-light.png" alt="">
+                    </div>
+
+                    <div class="tp-caption top-label"
+                        data-x="227"
+                        data-y="180"
+                        data-start="500"
+                        data-transform_in="y:[-300%];opacity:0;s:500;">DO YOU NEED A NEW</div>
+
+                    <div class="tp-caption"
+                        data-x="480"
+                        data-y="188"
+                        data-start="1000"
+                        data-transform_in="x:[300%];opacity:0;s:500;">
+                        <img src="<?php echo base_url();?>assets/img/slides/slide-title-border-light.png" alt="">
+                    </div>
+
+                    <div class="tp-caption main-label"
+                        data-x="135"
+                        data-y="210"
+                        data-start="1500"
+                        data-whitespace="nowrap"
+                        data-transform_in="y:[100%];s:500;"
+                        data-transform_out="opacity:0;s:500;"
+                        data-mask_in="x:0px;y:0px;">WEB DESIGN?</div>
+
+                    <div class="tp-caption bottom-label"
+                        data-x="185"
+                        data-y="280"
+                        data-start="2000"
+                        data-transform_in="y:[100%];opacity:0;s:500;">Check out our options and features.</div>
+
+                </li>
+                <li data-transition="fade">
+                    <img src="<?php echo base_url();?>assets/img/slides/slide-corporate-2.jpg"
+                        alt=""
+                        data-bgposition="right center"
+                        data-bgpositionend="center center"
+                        data-bgfit="cover"
+                        data-bgrepeat="no-repeat"
+                        data-kenburns="on"
+                        data-duration="9000"
+                        data-ease="Linear.easeNone"
+                        data-scalestart="110"
+                        data-scaleend="100"
+                        data-rotatestart="0"
+                        data-rotateend="0"
+                        data-offsetstart="0 0"
+                        data-offsetend="0 0"
+                        data-bgparallax="0"
+                        class="rev-slidebg">
+
+                    <div class="tp-caption featured-label"
+                        data-x="center"
+                        data-y="210"
+                        data-start="500"
+                        style="z-index: 5"
+                        data-transform_in="y:[100%];s:500;"
+                        data-transform_out="opacity:0;s:500;">WELCOME TO PORTO</div>
+
+                    <div class="tp-caption bottom-label"
+                        data-x="center"
+                        data-y="270"
+                        data-start="1000"
+                        data-transform_idle="o:1;"
+                        data-transform_in="y:[100%];z:0;rZ:-35deg;sX:1;sY:1;skX:0;skY:0;s:600;e:Power4.easeInOut;"
+                        data-transform_out="opacity:0;s:500;"
+                        data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
+                        data-splitin="chars"
+                        data-splitout="none"
+                        data-responsive_offset="on"
+                        style="font-size: 23px; line-height: 30px;"
+                        data-elementdelay="0.05">The #1 Selling HTML Site Template on ThemeForest</div>
+
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <section class="section m-none">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 center">
+                    <h2>Our <strong>Services</strong></h2>
+                </div>
+            </div>
+            <div class="row mt-lg">
+                <div class="col-xs-4">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img class="img-responsive mb-lg" src="<?php echo base_url();?>assets/img/icons/seo-grey.png" alt="">
+                        </div>
+                        <div class="col-md-8">
+                            <h4 class="mb-xs">SEO Optimization</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <p><a class="btn-flat btn-xs" href="#">View More <i class="fa fa-arrow-right"></i></a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-4">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img class="img-responsive mb-lg" src="<?php echo base_url();?>assets/img/icons/marketing-grey.png" alt="">
+                        </div>
+                        <div class="col-md-8">
+                            <h4 class="mb-xs">Marketing</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <p><a class="btn-flat btn-xs" href="#">View More <i class="fa fa-arrow-right"></i></a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-4">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img class="img-responsive mb-lg" src="<?php echo base_url();?>assets/img/icons/support-grey.png" alt="">
+                        </div>
+                        <div class="col-md-8">
+                            <h4 class="mb-xs">Support Services</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <p><a class="btn-flat btn-xs" href="#">View More <i class="fa fa-arrow-right"></i></a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section section-no-background m-none">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <img class="img-responsive mb-lg" src="<?php echo base_url();?>assets/img/office/our-office-9.jpg" alt="Office">
+                </div>
+                <div class="col-md-9">
+                    <h2 class="mb-lg">Our <strong>Company</strong></h2>
+                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut feugiat urna arcu. Mauris lobortis nulla ut aliquet interdum.</p>
+                    <p>Mauris lobortis nulla ut aliquet interdum. Donec commodo ac elit sed placerat. Mauris rhoncus est ac sodales gravida. In eros felis, elementum aliquam nisi vel, pellentesque faucibus nulla. Lorem ipsum dolor. <a class="btn-flat btn-xs" href="#">View More <i class="fa fa-arrow-right"></i></a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section m-none">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 center">
+                    <h2 class="mb-xs">Our <strong>Team</strong></h2>
+                </div>
+            </div>
+            <div class="row mt-lg">
+                <div class="col-md-2 col-xs-6 center mb-lg">
+                    <img src="<?php echo base_url();?>assets/img/team/team-11.jpg" class="img-responsive" alt="">
+                    <h5 class="mt-sm mb-none">Bruno Doe</h5>
+                    <p class="mb-none">CEO</p>
+                </div>
+                <div class="col-md-2 col-xs-6 center mb-lg">
+                    <img src="<?php echo base_url();?>assets/img/team/team-12.jpg" class="img-responsive" alt="">
+                    <h5 class="mt-sm mb-none">Jeff Doe</h5>
+                    <p class="mb-none">Developer</p>
+                </div>
+                <div class="col-md-2 col-xs-6 center mb-lg">
+                    <img src="<?php echo base_url();?>assets/img/team/team-13.jpg" class="img-responsive" alt="">
+                    <h5 class="mt-sm mb-none">Jessica Doe</h5>
+                    <p class="mb-none">Marketing</p>
+                </div>
+                <div class="col-md-2 col-xs-6 center mb-lg">
+                    <img src="<?php echo base_url();?>assets/img/team/team-14.jpg" class="img-responsive" alt="">
+                    <h5 class="mt-sm mb-none">Greg Doe</h5>
+                    <p class="mb-none">SEO</p>
+                </div>
+                <div class="col-md-2 col-xs-6 center mb-lg">
+                    <img src="<?php echo base_url();?>assets/img/team/team-15.jpg" class="img-responsive" alt="">
+                    <h5 class="mt-sm mb-none">Alex Doe</h5>
+                    <p class="mb-none">Support</p>
+                </div>
+                <div class="col-md-2 col-xs-6 center mb-lg">
+                    <img src="<?php echo base_url();?>assets/img/team/team-16.jpg" class="img-responsive" alt="">
+                    <h5 class="mt-sm mb-none">Melissa Doe</h5>
+                    <p class="mb-none">Support</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="parallax section section-text-light section-parallax section-center mt-none mb-none" data-plugin-parallax data-plugin-options="{'speed': 1.5}" data-image-src="img/parallax-3.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="owl-carousel owl-theme nav-bottom rounded-nav" data-plugin-options="{'items': 1, 'loop': false}">
+                        <div>
+                            <div class="col-md-12">
+                                <div class="testimonial testimonial-style-2 testimonial-with-quotes mb-none">
+                                    <div class="testimonial-author">
+                                        <img src="img/clients/client-1.jpg" class="img-responsive img-circle" alt="">
+                                    </div>
+                                    <blockquote>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget risus porta, tincidunt turpis at, interdum tortor. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce ante tellus, convallis non consectetur sed, pharetra nec ex.</p>
+                                    </blockquote>
+                                    <div class="testimonial-author">
+                                        <p><strong>John Smith</strong><span>CEO & Founder - Okler</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="col-md-12">
+                                <div class="testimonial testimonial-style-2 testimonial-with-quotes mb-none">
+                                    <div class="testimonial-author">
+                                        <img src="img/clients/client-1.jpg" class="img-responsive img-circle" alt="">
+                                    </div>
+                                    <blockquote>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget risus porta, tincidunt turpis at, interdum tortor. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    </blockquote>
+                                    <div class="testimonial-author">
+                                        <p><strong>John Smith</strong><span>CEO & Founder - Okler</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section mt-none section-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 center">
+                    <h2>Our <strong> Blog</strong></h2>
+                </div>
+            </div>
+            <div class="row mt-lg">
+                <div class="col-md-3">
+                    <img class="img-responsive" src="img/blog/blog-vintage-1.jpg" alt="Blog">
+                    <div class="recent-posts mt-md mb-lg">
+                        <article class="post">
+                            <h5><a class="text-dark" href="blog-post.html">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit vehicula est, in consequat.</p>
+                            <div class="post-meta">
+                                <span><i class="fa fa-calendar"></i> January 10, 2017 </span>
+                                <span><i class="fa fa-user"></i> By <a href="#">John Doe</a> </span>
+                                <span><i class="fa fa-tag"></i> <a href="#">Duis</a>, <a href="#">News</a> </span>
+                                <span><i class="fa fa-comments"></i> <a href="#">12 Comments</a></span>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <img class="img-responsive" src="img/blog/blog-vintage-2.jpg" alt="Blog">
+                    <div class="recent-posts mt-md mb-lg">
+                        <article class="post">
+                            <h5><a class="text-dark" href="blog-post.html">Lorem consectetur adipiscing elit.</a></h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit vehicula est, in consequat.</p>
+                            <div class="post-meta">
+                                <span><i class="fa fa-calendar"></i> January 10, 2017 </span>
+                                <span><i class="fa fa-user"></i> By <a href="#">John Doe</a> </span>
+                                <span><i class="fa fa-tag"></i> <a href="#">Duis</a>, <a href="#">News</a> </span>
+                                <span><i class="fa fa-comments"></i> <a href="#">12 Comments</a></span>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <img class="img-responsive" src="img/blog/blog-vintage-3.jpg" alt="Blog">
+                    <div class="recent-posts mt-md mb-lg">
+                        <article class="post">
+                            <h5><a class="text-dark" href="blog-post.html">Lorem dolor sit amet, consectetur adipiscing elit.</a></h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit vehicula est, in consequat.</p>
+                            <div class="post-meta">
+                                <span><i class="fa fa-calendar"></i> January 10, 2017 </span>
+                                <span><i class="fa fa-user"></i> By <a href="#">John Doe</a> </span>
+                                <span><i class="fa fa-tag"></i> <a href="#">Duis</a>, <a href="#">News</a> </span>
+                                <span><i class="fa fa-comments"></i> <a href="#">12 Comments</a></span>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <img class="img-responsive" src="img/blog/blog-vintage-4.jpg" alt="Blog">
+                    <div class="recent-posts mt-md mb-lg">
+                        <article class="post">
+                            <h5><a class="text-dark" href="blog-post.html">Lorem ipsum dot, consectetur adipiscing elit.</a></h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit vehicula est, in consequat.</p>
+                            <div class="post-meta">
+                                <span><i class="fa fa-calendar"></i> January 10, 2017 </span>
+                                <span><i class="fa fa-user"></i> By <a href="#">John Doe</a> </span>
+                                <span><i class="fa fa-tag"></i> <a href="#">Duis</a>, <a href="#">News</a> </span>
+                                <span><i class="fa fa-comments"></i> <a href="#">12 Comments</a></span>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</div>
---
+++ application/views/fe/includes/footer.php
@@ -0,0 +1,87 @@
+<footer class="short" id="footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <h4 class="heading-primary">About Us</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu pulvinar magna. Phasellus semper scelerisque purus, et semper nisl lacinia sit amet. Praesent venenatis turpis vitae purus semper, eget sagittis velit venenatis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos... <a href="#" class="btn-flat btn-xs">View More <i class="fa fa-arrow-right"></i></a></p>
+                            <hr class="light">
+                        </div>
+                        <div class="col-md-3 col-md-offset-1">
+                            <h5 class="mb-sm">Contact Us</h5>
+                            <span class="phone">(800) 123-4567</span>
+                            <p class="mb-none">International: (333) 456-6670</p>
+                            <p class="mb-none">Fax: (222) 531-8999</p>
+                            <ul class="list list-icons list-icons-sm">
+                                <li><i class="fa fa-envelope"></i> <a href="mailto:okler@okler.net">okler@okler.net</a></li>
+                            </ul>
+                            <ul class="social-icons">
+                                <li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                                <li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                                <li class="social-icons-linkedin"><a href="http://www.linkedin.com/" target="_blank" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-copyright">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-1">
+                                <a href="index.html" class="logo">
+                                    <img alt="Porto Website Template" class="img-responsive" src="img/logo-footer.png">
+                                </a>
+                            </div>
+                            <div class="col-md-11">
+                                <p>© Copyright 2017. All Rights Reserved.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+
+        <!-- Vendor -->
+        <script src="<?php echo base_url();?>assets/vendor/jquery/jquery.min.js"></script>
+        <script src="<?php echo base_url();?>assets/vendor/jquery.appear/jquery.appear.min.js"></script>
+        <script src="<?php echo base_url();?>assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+        <script src="<?php echo base_url();?>assets/vendor/jquery-cookie/jquery-cookie.min.js"></script>
+       <!--  <script src="master/style-switcher/style.switcher.js" id="styleSwitcherScript" data-base-path="" data-skin-src=""></script> -->
+        <script src="<?php echo base_url();?>assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script src="<?php echo base_url();?>assets/vendor/common/common.min.js"></script>
+        <script src="<?php echo base_url();?>assets/vendor/jquery.validation/jquery.validation.min.js"></script>
+        <script src="<?php echo base_url();?>assets/vendor/jquery.easy-pie-chart/jquery.easy-pie-chart.min.js"></script>
+        <script src="<?php echo base_url();?>assets/vendor/jquery.gmap/jquery.gmap.min.js"></script>
+        <script src="<?php echo base_url();?>assets/vendor/jquery.lazyload/jquery.lazyload.min.js"></script>
+        <script src="<?php echo base_url();?>assets/vendor/isotope/jquery.isotope.min.js"></script>
+        <script src="<?php echo base_url();?>assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+        <script src="<?php echo base_url();?>assets/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+        <script src="<?php echo base_url();?>assets/vendor/vide/vide.min.js"></script>
+
+        <!-- Theme Base, Components and Settings -->
+        <script src="<?php echo base_url();?>assets/js/theme.js"></script>
+
+        <!-- Current Page Vendor and Views -->
+        <script src="<?php echo base_url();?>assets/vendor/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+        <script src="<?php echo base_url();?>assets/vendor/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+        <!-- Current Page Vendor and Views -->
+        <script src="<?php echo base_url();?>assets/js/views/view.contact.js"></script>
+
+        <!-- Theme Custom -->
+        <script src="<?php echo base_url();?>assets/js/custom.js"></script>
+
+        <!-- Theme Initialization Files -->
+        <script src="<?php echo base_url();?>assets/js/theme.init.js"></script>
+
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','../../../www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-42715764-5', 'auto');
+            ga('send', 'pageview');
+        </script>
+        <script src="master/analytics/analytics.js"></script>
+
+    </body>
+</html>
